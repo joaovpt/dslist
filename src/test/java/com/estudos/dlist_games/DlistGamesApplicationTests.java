28dca4bbcd768707eb71fa217904a06f5ec34600
joaovpt <joaotassi18@gmail.com>
2025-05-19 14:59:37 -0300

    init: Project created

---
+++ src/test/java/com/estudos/dlist_games/DlistGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.dlist_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlistGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
